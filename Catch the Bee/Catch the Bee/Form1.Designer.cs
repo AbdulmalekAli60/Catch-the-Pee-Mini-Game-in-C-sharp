a041cc76403249486f454b9f5d82845ec7e77e00
AbdulmalekAli60 <98859591+AbdulmalekAli60@users.noreply.github.com>
2023-12-23 20:56:14 +0300

    Add files via upload

---
+++ Catch the Bee/Catch the Bee/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Catch_the_Bee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(472, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 108);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(474, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 566);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Catch the Bee/Catch the Bee/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Catch_the_Bee
+{
+    public partial class Form1 : Form
+    {
+        int time = 30;
+        int x;
+        int y;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moveTheBee(int x, int y)
+        {
+
+            button1.Location = new Point(x, y);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Location = new Point(x, y);
+            timer1.Stop();
+
+            MessageBox.Show("You Won");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x = random.Next(1, 1123);
+            y = random.Next(1, 622);
+
+
+            moveTheBee(x, y);
+            time--;
+            label1.Text = time.ToString();
+
+            if (time == 20)
+            {
+                timer1.Interval = 500;
+            }
+            else if (time == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("Time finished");
+            }
+        }
+    }
+}
